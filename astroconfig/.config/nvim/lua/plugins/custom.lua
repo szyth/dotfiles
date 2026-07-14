597ae54063b9337725fed78720e777cb3fa1575e
Syed Zia <46795709+szyth@users.noreply.github.com>
2026-07-14 08:41:43 +0530

    refactor

--- astroconfig/.config/nvim/lua/plugins/custom.lua
+++ astroconfig/.config/nvim/lua/plugins/custom.lua
@@ -1,4 +1,31 @@
-return {
+local function setup_file_watcher()
+  local uv = vim.uv or vim.loop
+  local function watch_file(bufnr)
+    if type(bufnr) ~= "number" or not vim.api.nvim_buf_is_valid(bufnr) then return end
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    if fname == "" then return end
+    
+    local fsevent = uv.new_fs_event()
+    fsevent:start(fname, {}, vim.schedule_wrap(function()
+      if vim.api.nvim_buf_is_valid(bufnr) and vim.fn.getbufvar(bufnr, "&modified") == 0 then
+        vim.cmd("checktime " .. bufnr)
+      end
+    end))
+    
+    vim.api.nvim_create_autocmd("BufUnload", {
+      buffer = bufnr,
+      once = true,
+      callback = function() if fsevent then fsevent:stop() end end,
+    })
+  end
+
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args) watch_file(args.buf) end,
+  })
+end
+
+-- 2. Calling the file watcher function
+setup_file_watcher()return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
